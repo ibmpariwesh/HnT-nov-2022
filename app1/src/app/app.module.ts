b3077d8aedd4576164de3d29d7e51df157158381
ibmpariwesh <ibmpariwesh@gmail.com>
2022-12-08 17:36:22 +0530

    pipe

--- app1/src/app/app.module.ts
+++ app1/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { UserformComponent } from './userform/userform.component';
 import { OrderformComponent } from './orderform/orderform.component';
 import { HttpClientModule } from "@angular/common/http";
+import { EllipsisPipe } from './ellipsis.pipe';
 @NgModule({//decorator
   declarations: [
     AppComponent,
     UserformComponent,
-    OrderformComponent
+    OrderformComponent,
+    EllipsisPipe
   ],
   imports: [
     BrowserModule, FormsModule, HttpClientModule
