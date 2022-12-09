8b89878afa8e923b01c3625d42c6ee5d6e4f8fee
ibmpariwesh <ibmpariwesh@gmail.com>
2022-12-09 14:22:35 +0530

    basic routing

--- app1/src/app/app.module.ts
+++ app1/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -7,6 +7,11 @@ import { UserformComponent } from './userform/userform.component';
 import { OrderformComponent } from './orderform/orderform.component';
 import { HttpClientModule } from "@angular/common/http";
 import { EllipsisPipe } from './ellipsis.pipe';
+import { Route, RouterModule } from '@angular/router';
+
+const routes: Route[] = [
+  { path: 'userform', component: UserformComponent }
+]
 @NgModule({//decorator
   declarations: [
     AppComponent,
@@ -15,7 +20,7 @@ import { EllipsisPipe } from './ellipsis.pipe';
     EllipsisPipe
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule
+    BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
